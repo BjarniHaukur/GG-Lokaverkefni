edd6396c12e045fbd5b919d86e8f068adeb107e2
Bobbi11 <bjarnihaukur11@gmail.com>
2021-11-12 16:39:38 +0000

    Data_loader tilbuid

--- data_loader.py
+++ data_loader.py
@@ -10,36 +10,28 @@ from PIL import Image
 
 class MyDataLoader(object):   
 
-    def __init__(self, dirName, jsonName="cat_to_name.json", trainName="train", testName="test"):
+    def __init__(self, dirName, jsonName="cat_to_name.json", trainName="train", testName="test", valName="val"):
         self.dirName = dirName
         self.jsonName = jsonName
         self.trainName = trainName
         self.testName = testName
+        self.valName = valName
 
         self.rootPath  = os.getcwd()
         self.dataPath  = os.path.join(self.rootPath, self.dirName)
+
         self.trainPath = os.path.join(self.rootPath, self.trainName)
+        self.XPath = os.path.join(self.trainPath, "X")
+        self.yPath = os.path.join(self.trainPath, "y")
+        
         self.failPath  = os.path.join(self.trainPath, "fail")
-        self.testPath  = os.path.join(self.rootPath, self.testName)
+
 
         with open(self.jsonName) as json_file:
             self.nameDict = json.load(json_file)
-        
-        # TODO 1 ekki gera svona (fer eftir röð aðgerða hvort þetta virki)
-        self.numTrainImages = 0
-        self.numTestImages = 0
 
         self.norm_size = 0
         
-
-    def __resizable(self, img_size, aspect_ratio, tolerance):
-        width = img_size[0]
-        height = img_size[1]
-
-        if width >= height:
-            return aspect_ratio - tolerance <= width/height <= aspect_ratio + tolerance
-        return aspect_ratio - tolerance <= height/width <= aspect_ratio + tolerance
-
 
     def normalize_train_data(self, norm_size=(500,500), aspect_ratio = 1.5, tolerance = 0.5):
         self.norm_size=norm_size
@@ -50,20 +42,21 @@ class MyDataLoader(object):
             pass
 
         os.mkdir(self.trainPath)
+        os.mkdir(self.yPath)
         os.mkdir(self.failPath)
 
         for (_, dirNames, _) in walk(self.dataPath):
 
             for category in dirNames:
                 readPath = os.path.join(self.dataPath, category)
-                writePath = os.path.join(self.trainPath, category)
+                writePath = os.path.join(self.yPath, category)
                 os.mkdir(writePath)
 
                 for (_,_,fileNames) in walk(readPath):
 
                     imgNumber = 1
                     for imgName in fileNames:
-                        img = Image.open(readPath+"\\"+imgName).convert('L')
+                        img = Image.open(readPath+"\\"+imgName)
                         if self.__resizable(img.size, aspect_ratio, tolerance):
                             img = img.resize(norm_size)
                             img.save(os.path.join(writePath, self.nameDict[category]+str(imgNumber)+".jpg"), "JPEG")
@@ -71,34 +64,81 @@ class MyDataLoader(object):
                             img.save(os.path.join(self.failPath, self.nameDict[category]+str(imgNumber)+".jpg"), "JPEG")
                         imgNumber = imgNumber + 1
 
-    def train_test_split(self, ratio_test):
-        try:
-            shutil.rmtree(self.testPath)
-        except:
-            pass
+    def make_colorization_data(self):
+        if os.path.isdir(self.yPath):
+            os.mkdir(self.XPath)
+            for (_, dirNames, _) in walk(self.yPath):
 
-        os.mkdir(self.testPath)
+                for category in dirNames:
+                    readPath = os.path.join(self.yPath, category)
+                    writePath = os.path.join(self.XPath, category)
+                    os.mkdir(writePath)
 
-        for (_, dirNames, _) in walk(self.trainPath):
+                    for (_,_,fileNames) in walk(readPath):
+
+                        for imgName in fileNames:
+                            img = Image.open(readPath+"\\"+imgName).convert('L')
+                            img.save(os.path.join(writePath, imgName), "JPEG")
+        else:
+            print("No such directory of normalized images. Have you executed normalize_train_data?")
+    
+
+
+    def to_numpy_array(self, color):
+        color_settings = {"rgb":3, "grayscale":1}
+        num_color_channesl = color_settings[color]
+        X_train, y_train = self.__read_classification(self.trainPath, self.__dir_size(self.trainPath), num_color_channesl)                
+        X_test, y_test = self.__read_classification(self.testPath, self.__dir_size(self.testPath), num_color_channesl)        
+        return X_train, y_train, X_test, y_test
+
+    
+    def __resizable(self, img_size, aspect_ratio, tolerance):
+        width = img_size[0]
+        height = img_size[1]
+
+        if width >= height:
+            return aspect_ratio - tolerance <= width/height <= aspect_ratio + tolerance
+        return aspect_ratio - tolerance <= height/width <= aspect_ratio + tolerance
+
+    def __dir_size(self, path):
+        if(os.path.isdir(path)):
+            size = 0
+            for (_, dirNames, _) in walk(path):
+                for dirName in dirNames:
+                    if dirName == "fail": continue
+                    readPath = os.path.join(self.trainPath, dirName)
+                    for (_, _, fileNames) in walk(readPath):
+                        size = size + len(fileNames)
+            return size-1
+        else:
+            print("Not a valid path to directory")
+            return 0
+
+    def __read_classification(self, path, size, num_color_channels=1):
+        if num_color_channels==1:
+            X = np.ndarray(shape=(size, self.norm_size[0], self.norm_size[1]), dtype=np.short)
+        else:
+            X = np.ndarray(shape=(size, self.norm_size[0], self.norm_size[1], num_color_channels), dtype=np.short)
+        y = np.ndarray(shape=size)
+        iter = 0
+        for (_, dirNames, _) in walk(path):
             for dirName in dirNames:
-                newTrainDir = os.path.join(self.trainPath, dirName)
-                newTestDir = os.path.join(self.testPath, dirName)
-                os.mkdir(newTestDir)
-
-                for (_, _, fileNames) in walk(newTrainDir):
+                if dirName=="fail": continue
+                readPath = os.path.join(path, dirName)
+                for (_, _, fileNames) in walk(readPath):
                     for name in fileNames:
-                        if random.random() < ratio_test:
-                            oldPath = os.path.join(newTrainDir, name)
-                            newPath = os.path.join(newTestDir, name)
-                            shutil.move(oldPath, newPath)
-                            # TODO 1
-                            self.numTestImages = self.numTestImages + 1
-                        else:
-                            # TODO 1
-                            self.numTrainImages = self.numTrainImages + 1
-
-    def __read_to_arrays(self, path, size):
-        X = np.ndarray(shape=(size, self.norm_size[0], self.norm_size[1]))
+                        img = Image.open(readPath+"\\"+name)
+                        X[iter] = np.array(img)
+                        y[iter] = dirName
+                        iter = iter + 1
+        return X, y
+    
+    # TODO
+    def __read_colorization(self, path, size, num_color_channels=1):
+        if num_color_channels==1:
+            X = np.ndarray(shape=(size, self.norm_size[0], self.norm_size[1]), dtype=np.short)
+        else:
+            X = np.ndarray(shape=(size, self.norm_size[0], self.norm_size[1], num_color_channels), dtype=np.short)
         y = np.ndarray(shape=size)
         iter = 0
         for (_, dirNames, _) in walk(path):
@@ -113,10 +153,25 @@ class MyDataLoader(object):
                         iter = iter + 1
         return X, y
 
-    def to_numpy_array(self):
-        X_train, y_train = self.__read_to_arrays(self.trainPath, self.numTrainImages)                
-        X_test, y_test = self.__read_to_arrays(self.testPath, self.numTestImages)        
-        return X_train, y_train, X_test, y_test
-
     
 
+# def train_test_val_split(self, ratio_test):
+    #     try:
+    #         shutil.rmtree(self.testPath)
+    #     except:
+    #         pass
+
+    #     os.mkdir(self.testPath)
+
+    #     for (_, dirNames, _) in walk(self.trainPath):
+    #         for dirName in dirNames:
+    #             newTrainDir = os.path.join(self.trainPath, dirName)
+    #             newTestDir = os.path.join(self.testPath, dirName)
+    #             os.mkdir(newTestDir)
+
+    #             for (_, _, fileNames) in walk(newTrainDir):
+    #                 for name in fileNames:
+    #                     if random.random() < ratio_test:
+    #                         oldPath = os.path.join(newTrainDir, name)
+    #                         newPath = os.path.join(newTestDir, name)
+    #                         shutil.move(oldPath, newPath)
