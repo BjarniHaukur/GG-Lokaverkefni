9a146598d4f17eeb0c1111611150689860a9b96c
Bobbi11 <bjarnihaukur11@gmail.com>
2021-11-14 19:07:20 +0000

    wth

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-from data_loader import MyDataLoader
 from sklearn.model_selection import train_test_split
 from keras.layers import Input, Conv2D, BatchNormalization, UpSampling2D, Dense, InputLayer
 from keras.models import Model
@@ -6,60 +5,94 @@ from keras.regularizers import l2
 from tensorflow.keras.optimizers.schedules import PolynomialDecay
 from tensorflow.keras.optimizers import Adam
 from keras import Sequential
-from nn_helper import save_model, load_model
 import numpy as np
 from PIL import Image
-import os
 
-print()
-print(os.getcwd())
-print()
+from data_loader import MyDataLoader
+from nn_helper import save_model, load_model
+from neural_nets import NeuralNets
+from skimage import color
 
-dl = MyDataLoader("combine2", (128,128))
-
-
-X, y = dl.get_lab_data()
+import tensorflow as tf
+import sys
 
 
-X_lab = X/100
-y_lab = y/128
+def main():
+
+    # config = tf.ConfigProto()
+    # config.gpu_options.allow_growth = True
+    # config.gpu_options.per_process_gpu_memory_fraction = 0.8
+    # tf.Session(config=config)
 
 
+    gpus = tf.config.list_physical_devices('GPU')
 
-X_train, X_test, y_train, y_test = train_test_split(X_lab, y_lab, test_size = 0.3, random_state = 3)
-X_test, X_val, y_test, y_val = train_test_split(X_test, y_test, test_size = 0.33, random_state = 3)
+    if gpus:
+        for gpu in gpus:
+            tf.config.experimental.set_memory_growth(gpu,True)
 
+    norm = (128,128)
+    dl = MyDataLoader("combine2", norm)
+    nn = NeuralNets(norm)
 
+    X = dl.numpy_load("X")
+    y = dl.numpy_load("y")
 
-input_shape = (128, 128, 1)
-kernel = (2, 2)
+    X_lab = X/100
+    y_lab = y/128
 
-model = Sequential()
-model.add(InputLayer(input_shape=input_shape))
-model.add(Conv2D(64, kernel, activation='relu', padding='same'))
-model.add(Conv2D(64, kernel, activation='relu', padding='same', strides=2))
-model.add(Conv2D(128, kernel, activation='relu', padding='same'))
-model.add(UpSampling2D((2,2)))
-model.add(Conv2D(128, kernel, activation='relu', padding='same', strides=2))
-model.add(Conv2D(256, kernel, activation='relu', padding='same'))
-model.add(Conv2D(256, kernel, activation='relu', padding='same', strides=2))
-model.add(Conv2D(512, kernel, activation='relu', padding='same'))
-model.add(Conv2D(256, kernel, activation='relu', padding='same'))
-model.add(Conv2D(128, kernel, activation='relu', padding='same'))
-model.add(UpSampling2D((2,2)))
-model.add(Conv2D(64, kernel, activation='relu', padding='same'))
-model.add(UpSampling2D((2, 2)))
-model.add(Conv2D(32, kernel, activation='relu', padding='same'))
-model.add(Conv2D(2, kernel, activation='sigmoid', padding='same'))
+    X_train, X_test, y_train, y_test = train_test_split(X_lab, y_lab, test_size = 0.3, random_state = 3)
+    X_test, X_val, y_test, y_val = train_test_split(X_test, y_test, test_size = 0.33, random_state = 3)
 
-model.compile(optimizer='rmsprop', loss='mse')
-
-# model = load_model("model_a")
+    # print(X_lab.shape)
+    # print(y_lab.shape)
+    # print(np.max(X_lab))
+    # print(np.max(y_lab))
+    # print(np.min(X_lab))
+    # print(np.min(y_lab))
 
 
-num_epochs = 10
-nr = 42
-canvas = np.zeros((128,128,3))
-for i in range(num_epochs):
-    model.fit(X_train, y_train, batch_size = 64, epochs = 10)
-    save_model(model, "model_a", brave=True)
+    model = nn.model_c()
+    model.compile(optimizer=Adam(1e-3), loss='msle')
+
+    # model = load_model("model_c")
+    
+
+    num_epochs = 20
+
+    percent = 0.6
+    size = X_train.shape[0]
+
+    canvas = np.zeros((norm[0],norm[1],3))
+    nr = 69
+    test_image = X_test[nr]
+    test_image = np.expand_dims(test_image, axis=0)
+    for i in range(num_epochs):
+        canvas = np.zeros((norm[0],norm[1],3))
+
+        index_array = np.random.choice(size, int(size*percent))
+        X_batch = X_train[index_array]
+        y_batch = y_train[index_array]
+
+        model.fit(X_batch, y_batch, batch_size = 32, epochs = 5)
+        save_model(model, "model_c", brave=True)
+
+        pred = model.predict(test_image)
+        canvas[:,:,0] = test_image[0,:,:,0]*100
+        canvas[:,:,1:] = pred*128
+        canvas = color.lab2rgb(canvas)*255
+        canvas = canvas.astype(np.uint8)
+        img = Image.fromarray(canvas)
+        img.save(f'mynd{i}.png')
+    
+    true_image = np.zeros((norm[0],norm[1],3))
+    true_image[:,:,0] = X_test[nr,:,:,0]*100
+    true_image[:,:,1:] = y_test[nr,:,:,1:]*128
+    true_image = color.lab2rgb(true_image)*255
+    true_image = true_image.astype(np.uint8)
+    true_image = Image.fromarray(true_image)
+    true_image.save("true.png")
+
+
+if __name__ == '__main__':
+    main()
--- neural_nets.py
+++ neural_nets.py
@@ -1,5 +1,5 @@
 
-from keras.layers import Input, Conv2D, BatchNormalization, UpSampling2D, Dense, InputLayer
+from keras.layers import *
 from keras.models import Model
 from keras.regularizers import l2
 from tensorflow.keras.optimizers.schedules import PolynomialDecay
@@ -33,3 +33,139 @@ class NeuralNets(object):
         model.add(Conv2D(2, kernel, activation='sigmoid', padding='same'))
 
         return model
+
+    def model_b(self):
+        return self.__ResNet34()
+
+
+    def model_c(self, kernel=2):
+        l2_reg = l2(1e-3)
+        input_tensor = Input(shape=(self.norm_size[0], self.norm_size[1], 1))
+        x = Conv2D(64, (kernel, kernel), activation='relu', padding='same', name='conv1_1', kernel_initializer="he_normal",
+                kernel_regularizer=l2_reg)(input_tensor)
+        x = Conv2D(64, (kernel, kernel), activation='relu', padding='same', name='conv1_2', kernel_initializer="he_normal",
+                kernel_regularizer=l2_reg, strides=(2, 2))(x)
+        x = BatchNormalization()(x)
+
+        x = Conv2D(128, (kernel, kernel), activation='relu', padding='same', name='conv2_1', kernel_initializer="he_normal",
+                kernel_regularizer=l2_reg)(x)
+        x = Conv2D(128, (kernel, kernel), activation='relu', padding='same', name='conv2_2', kernel_initializer="he_normal",
+                kernel_regularizer=l2_reg,
+                strides=(2, 2))(x)
+        x = BatchNormalization()(x)
+
+        x = Conv2D(256, (kernel, kernel), activation='relu', padding='same', name='conv3_1',
+                kernel_initializer="he_normal", kernel_regularizer=l2_reg)(x)
+        x = Conv2D(256, (kernel, kernel), activation='relu', padding='same', name='conv3_2',
+                kernel_initializer="he_normal", kernel_regularizer=l2_reg)(x)
+        x = Conv2D(256, (kernel, kernel), activation='relu', padding='same', name='conv3_3', kernel_initializer="he_normal",
+                strides=(2, 2))(x)
+        x = BatchNormalization()(x)
+
+        x = Conv2D(512, (kernel, kernel), activation='relu', padding='same', name='conv4_1',
+                kernel_initializer="he_normal", kernel_regularizer=l2_reg)(x)
+        x = Conv2D(512, (kernel, kernel), activation='relu', padding='same', name='conv4_2',
+                kernel_initializer="he_normal", kernel_regularizer=l2_reg)(x)
+        x = Conv2D(512, (kernel, kernel), activation='relu', padding='same', name='conv4_3',
+                kernel_initializer="he_normal", kernel_regularizer=l2_reg)(x)
+        x = BatchNormalization()(x)
+
+        x = Conv2D(512, (kernel, kernel), activation='relu', padding='same', dilation_rate=2, name='conv5_1',
+                kernel_initializer="he_normal", kernel_regularizer=l2_reg)(x)
+        x = Conv2D(512, (kernel, kernel), activation='relu', padding='same', dilation_rate=2, name='conv5_2',
+                kernel_initializer="he_normal", kernel_regularizer=l2_reg)(x)
+        x = Conv2D(512, (kernel, kernel), activation='relu', padding='same', dilation_rate=2, name='conv5_3',
+                kernel_initializer="he_normal", kernel_regularizer=l2_reg)(x)
+        x = BatchNormalization()(x)
+
+        x = Conv2D(512, (kernel, kernel), activation='relu', padding='same', dilation_rate=2, name='conv6_1',
+                kernel_initializer="he_normal", kernel_regularizer=l2_reg)(x)
+        x = Conv2D(512, (kernel, kernel), activation='relu', padding='same', dilation_rate=2, name='conv6_2',
+                kernel_initializer="he_normal", kernel_regularizer=l2_reg)(x)
+        x = Conv2D(512, (kernel, kernel), activation='relu', padding='same', dilation_rate=2, name='conv6_3',
+                kernel_initializer="he_normal", kernel_regularizer=l2_reg)(x)
+        x = BatchNormalization()(x)
+
+        x = Conv2D(256, (kernel, kernel), activation='relu', padding='same', name='conv7_1',
+                kernel_initializer="he_normal", kernel_regularizer=l2_reg)(x)
+        x = Conv2D(256, (kernel, kernel), activation='relu', padding='same', name='conv7_2',
+                kernel_initializer="he_normal", kernel_regularizer=l2_reg)(x)
+        x = Conv2D(256, (kernel, kernel), activation='relu', padding='same', name='conv7_3',
+                kernel_initializer="he_normal", kernel_regularizer=l2_reg)(x)
+        x = BatchNormalization()(x)
+
+        x = UpSampling2D(size=(2, 2))(x)
+        x = Conv2D(128, (kernel, kernel), activation='relu', padding='same', name='conv8_1',
+                kernel_initializer="he_normal", kernel_regularizer=l2_reg)(x)
+        x = Conv2D(128, (kernel, kernel), activation='relu', padding='same', name='conv8_2',
+                kernel_initializer="he_normal", kernel_regularizer=l2_reg)(x)
+        x = Conv2D(128, (kernel, kernel), activation='relu', padding='same', name='conv8_3',
+                kernel_initializer="he_normal", kernel_regularizer=l2_reg)(x)
+        x = BatchNormalization()(x)
+        x = (UpSampling2D((2,2)))(x)
+        x = (Conv2D(64, kernel, activation='relu', padding='same'))(x)
+        x = (UpSampling2D((2, 2)))(x)
+        x = (Conv2D(32, kernel, activation='relu', padding='same'))(x)
+
+        outputs = Conv2D(2, (1, 1), activation='softmax', padding='same', name='pred')(x)
+
+        model = Model(inputs=input_tensor, outputs=outputs, name="ColorNet")
+        return model
+
+    def __res_block(self, inputs, filters=64):
+        x = Conv2D(filters, (3, 3), padding='same')(inputs)
+        x = BatchNormalization()(x)
+        x = ReLU()(x)
+        x = Conv2D(filters, (3, 3), padding='same')(x)
+        x = BatchNormalization()(x)
+        x = Add()([x, inputs])
+        x = ReLU()(x)
+
+        return x
+
+
+    def __ResNet34(self):
+        img_input = Input(shape=(self.norm_size[0], self.norm_size[1], 1))
+
+        x = Conv2D(64, (7, 7), strides=2, padding='same')(img_input)
+        x = MaxPooling2D()(x)
+
+        x = self.__res_block(x, filters=64)
+        x = self.__res_block(x, filters=64)
+        x = self.__res_block(x, filters=64)
+
+        x = Conv2D(128, (3, 3), padding='same')(x)
+        x = BatchNormalization()(x)
+        x = ReLU()(x)
+        x = MaxPooling2D()(x)
+        x = Conv2D(128, (3, 3), padding='same')(x)
+        x = BatchNormalization()(x)
+        x = ReLU()(x)
+        x = self.__res_block(x, filters=128)
+        x = self.__res_block(x, filters=128)
+        x = self.__res_block(x, filters=128)
+
+        x = Conv2D(256, (3, 3), padding='same')(x)
+        x = BatchNormalization()(x)
+        x = ReLU()(x)
+        x = MaxPooling2D()(x)
+        x = Conv2D(256, (3, 3), padding='same')(x)
+        x = BatchNormalization()(x)
+        x = ReLU()(x)
+        x = self.__res_block(x, filters=256)
+        x = self.__res_block(x, filters=256)
+        x = self.__res_block(x, filters=256)
+        x = self.__res_block(x, filters=256)
+        x = self.__res_block(x, filters=256)
+
+        x = Conv2D(512, (3, 3), padding='same')(x)
+        x = BatchNormalization()(x)
+        x = ReLU()(x)
+        x = MaxPooling2D()(x)
+        x = Conv2D(512, (3, 3), padding='same')(x)
+        x = BatchNormalization()(x)
+        x = ReLU()(x)
+        x = self.__res_block(x, filters=512)
+        x = self.__res_block(x, filters=512)
+
+        return Model(img_input, x)
