58abb8342a4bc936feec757b35c99796d320c956
Bobbi11 <bjarnihaukur11@gmail.com>
2021-11-16 18:11:55 +0000

    betra

---
+++ helper_funcs.py
@@ -0,0 +1,99 @@
+import os
+import shutil
+from PIL import Image
+from skimage import color
+import numpy as np
+from tqdm import tqdm
+from tensorflow import keras
+import matplotlib.pyplot as plt
+from tensorflow.keras import layers
+from tensorflow.keras.models import Sequential
+
+def __prompt():
+    val = input("y/n?")
+    if val=='y':
+        return True
+    if val=='n':
+        return False
+    else:
+        __prompt()
+
+def save_model(model, dirName, brave=False):
+    root = os.getcwd()
+    myDir = os.path.join(root, dirName)
+    if os.path.isdir(myDir):
+        if not brave: print("Directory already exists, overwrite?")
+        if brave or __prompt():
+            shutil.rmtree(myDir)
+            os.mkdir(myDir)
+            model.save(dirName)
+        else:
+            return
+    else:
+        os.mkdir(myDir)
+        model.save(dirName)
+
+
+def load_model(dirName):
+    root = os.getcwd()
+    myDir = os.path.join(root, dirName)
+    if os.path.isdir(myDir):
+        return keras.models.load_model(dirName)
+    else:
+        print("No such directory")
+
+def delete_model(dirName):
+    root = os.getcwd()
+    myDir = os.path.join(root, dirName)
+    shutil.rmtree(myDir)
+
+def show_images(X, predictions):
+    num_images = predictions.shape[0]
+    height = predictions.shape[1]
+    width = predictions.shape[2]
+
+    if not X.shape[0]==num_images:
+        print("Arrays do not have the same dimensions")
+        return
+
+    for i in range(num_images):
+        canvas = np.zeros((height, width, 3))
+        canvas[:,:,0] = X[i,:,:,0]*100
+        canvas[:,:,1:] = predictions[i]*128
+        canvas = (color.lab2rgb(canvas)*255).astype(np.uint8)
+        img = Image.fromarray(canvas)
+        img.show()
+
+def save_images(X, predictions, name="mynd", enumerate=None):
+    num_images = predictions.shape[0]
+    height = predictions.shape[1]
+    width = predictions.shape[2]
+
+    if not X.shape[0]==num_images:
+        print("Arrays do not have the same dimensions")
+        return
+
+    root = os.getcwd()
+    writePath = os.path.join(root, "myndir")
+    if not os.path.isdir(writePath):
+        os.mkdir(writePath)
+
+    for i in range(num_images):
+        canvas = np.zeros((height, width, 3))
+        canvas[:,:,0] = X[i,:,:,0]*100
+        canvas[:,:,1:] = predictions[i]*128
+        canvas = (color.lab2rgb(canvas)*255).astype(np.uint8)
+        img = Image.fromarray(canvas)
+        if enumerate:
+            img.save(os.path.join(writePath, f"{name}{enumerate+1}_{i+1}.jpg"), 'JPEG') 
+        else:
+            img.save(os.path.join(writePath, f"{name}{i+1}.jpg"), 'JPEG')
+
+def map_to(arr):
+    func = lambda x: (x+1)/2
+    return func(arr)
+    #np.vectorize(func)(arr)
+
+def map_from(arr):
+    func = lambda x: 2*x-1
+    return func(arr)
--- main.py
+++ main.py
@@ -5,12 +5,13 @@ from keras.regularizers import l2
 from sklearn.utils import validation
 from tensorflow.keras.optimizers.schedules import PolynomialDecay
 from tensorflow.keras.optimizers import *
+from tensorflow.keras.losses import *
 from keras import Sequential
 import numpy as np
 from PIL import Image
 
 from data_loader import MyDataLoader
-from nn_helper import save_model, load_model
+from helper_funcs import *
 from neural_nets import NeuralNets
 from skimage import color
 
@@ -21,12 +22,6 @@ import sys
 
 
 def main():
-
-    # config = tf.ConfigProto()
-    # config.gpu_options.allow_growth = True
-    # config.gpu_options.per_process_gpu_memory_fraction = 0.8
-    # tf.Session(config=config)
-
     gpus = tf.config.list_physical_devices('GPU')
 
     if gpus:
@@ -37,11 +32,19 @@ def main():
     dl = MyDataLoader("combine2", norm)
     nn = NeuralNets(norm)
 
+    #dl.normalize_train_data(norm_size=norm)
+
+    #X, y = dl.get_lab_data()
+
+    #dl.numpy_dump(X, "X")
+    #dl.numpy_dump(y, "y")
+
     X = dl.numpy_load("X")
     y = dl.numpy_load("y")
 
-    X_lab = X/100
-    y_lab = y/128
+
+    X_lab = X
+    y_lab = map_to(y)
 
     X_train, X_test, y_train, y_test = train_test_split(X_lab, y_lab, test_size = 0.3, random_state = 3)
     X_test, X_val, y_test, y_val = train_test_split(X_test, y_test, test_size = 0.33, random_state = 3)
@@ -53,11 +56,14 @@ def main():
     # print(np.min(X_lab))
     # print(np.min(y_lab))
 
-
-    model = nn.model_e()
-    model.compile(optimizer=RMSprop(), loss='mse')
-
-    #model = load_model("model_d")
+    # decay_schedule = PolynomialDecay(initial_learning_rate=1e-3,
+    #                                 decay_steps=30000,
+    #                                 end_learning_rate=1e-5)
+    model = load_model("model_e")
+    #model = nn.model_e()
+    model.compile(optimizer=RMSprop(), loss=MeanAbsoluteError())
+    # (optimizer=Adam(learning_rate=decay_schedule), loss='mse')
+    # model = load_model("model_d")
     
 
     num_epochs = 30
@@ -67,36 +73,21 @@ def main():
     # patience = 5
     # callback = EarlyStopping(monitor="val_loss", patience=patience)
 
-    canvas = np.zeros((norm[0],norm[1],3))
+    log_dir = "logs/fit/" + "model_e"
+    tensorboard_callback = tf.keras.callbacks.TensorBoard(log_dir=log_dir, histogram_freq=1)
+
+ 
     nr = 101
     test_image = X_test[nr]
     test_image = np.expand_dims(test_image, axis=0)
     for i in range(num_epochs):
-        canvas = np.zeros((norm[0],norm[1],3))
 
-        # index_array = np.random.choice(size, int(size*percent))
-        # X_batch = X_train[index_array]
-        # y_batch = y_train[index_array]
-
-        model.fit(X_train, y_train, batch_size = 64, epochs = 5, validation_data=(X_val, y_val), verbose=2)#, callbacks=[callback])
+        model.fit(X_train, y_train, batch_size = 32, epochs = 5, validation_data=(X_val, y_val), verbose=2, callbacks=[tensorboard_callback])#, callbacks=[callback])
         save_model(model, "model_e", brave=True)
 
         pred = model.predict(test_image)
-        canvas[:,:,0] = test_image[0,:,:,0]*100
-        canvas[:,:,1:] = pred*128
-        canvas = color.lab2rgb(canvas)*255
-        canvas = canvas.astype(np.uint8)
-        img = Image.fromarray(canvas)
-        img.save(f'mynd{i}.png')
+        save_images(test_image, map_from(pred), name="epoch", enumerate=i)
     
-    true_image = np.zeros((norm[0],norm[1],3))
-    true_image[:,:,0] = X_test[nr,:,:,0]*100
-    true_image[:,:,1:] = y_test[nr,:,:,1:]*128
-    true_image = color.lab2rgb(true_image)*255
-    true_image = true_image.astype(np.uint8)
-    true_image = Image.fromarray(true_image)
-    true_image.save("true.png")
-
 
 if __name__ == '__main__':
     main()
